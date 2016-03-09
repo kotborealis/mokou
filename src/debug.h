4775da2b4cc2fba1257e5945f2f57d72bf851bbe
kotborealis <kotborealis@gmail.com>
2016-03-09 21:20:39 +0300

    Added debug utils (log)

--- src/debug.h
+++ src/debug.h
@@ -6,15 +6,7 @@
 #include <iomanip>
 
 namespace debug{
-	std::string get_time(){
-		auto t = std::time(0);
-		return "["+std::to_string(t)+"]";
-	}
-
-	void i_log(std::string s){
-		std::cout<<get_time()<<"[d_log]: "<<s<<"\n";
-	}
-	void e_log(std::string s){
-		std::cout<<get_time()<<"[e_log]: "<<s<<"\n";
-	}
+	std::string get_time();
+	void i_log(std::string s);
+	void e_log(std::string s);
 }
